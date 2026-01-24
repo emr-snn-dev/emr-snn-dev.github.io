d1bb4bb2e2057c5e9eedde875eb858e647a1b05d
emr-snn-dev <dev.snn.emc@gmail.com>
2026-01-24 12:00:07 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,12 +1,10 @@
 const menuBtn = document.getElementById('menu-btn');
 const navList = document.getElementById('nav-list');
 
-// メニュー開閉
 menuBtn.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
 
-// 項目クリックで閉じる
 document.querySelectorAll('#nav-list a').forEach(link => {
     link.addEventListener('click', () => {
         navList.classList.remove('active');
